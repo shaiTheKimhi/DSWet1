f1d5d147646c7a2dca15993257ef8e34b585abe7
yaaraKa <yaara.karmy@gmail.com>
2019-12-08 20:12:00 +0200

    fix

--- library.cpp
+++ library.cpp
@@ -8,22 +8,37 @@ void *Init() {
 }
 
 StatusType AddDataCenter(void *DS, int dataCenterID, int numOfServers) {
+    if (DS == nullptr) {
+        return INVALID_INPUT;
+    }
     return ((DataCenterSystem *)DS)->AddDataCenter(dataCenterID,numOfServers);
 }
 
 StatusType RemoveDataCenter(void *DS, int dataCenterID) {
+    if (DS == nullptr) {
+        return INVALID_INPUT;
+    }
     return ((DataCenterSystem *)DS)->RemoveDataCenter(dataCenterID);
 }
 
 StatusType RequestServer(void *DS, int dataCenterID, int serverID, int os, int *assignedID) {
+    if (DS == nullptr) {
+        return INVALID_INPUT;
+    }
     return ((DataCenterSystem *)DS)->RequestServer(dataCenterID,serverID,os,assignedID);
 }
 
 StatusType FreeServer(void *DS, int dataCenterID, int serverID) {
+    if (DS == nullptr) {
+        return INVALID_INPUT;
+    }
     return ((DataCenterSystem *)DS)->FreeServer(dataCenterID,serverID);
 }
 
 StatusType GetDataCentersByOS(void *DS, int os, int **dataCenters, int* numOfDataCenters) {
+    if (DS == nullptr) {
+        return INVALID_INPUT;
+    }
     return ((DataCenterSystem *)DS)->GetDataCentersByOS(os,dataCenters,numOfDataCenters);
 }
 
